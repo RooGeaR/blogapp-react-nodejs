ec72c29932755feabcde5828d914990cb01a3fa3
Roger Acosta <roger@mailvibes.io>
2024-02-19 15:19:10 -0600

    Design Navbar and Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Logo from "../img/blog-app-logo.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex items-center justify-between mt-20 p-5 bg-orange-400 text-xs">
+      <img className="w-14" src={Logo} alt="Blog Logo App" />
+      <span>Made with <b>React.js</b></span>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Logo from "../img/blog-app-logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="flex justify-between pl-0 pr-2.5">
-        <div>Logo</div>
-        <div>Links</div>
+      <div className="flex justify-between items-center pl-0 pr-2.5">
+        <div>
+          <img src={Logo} className="w-28" alt="Logo Blog App" />
+        </div>
+        <div className="flex items-center gap-2.5">
+          <span className="font-light bg-orange-500 rounded-full w-12 h-12 flex items-center justify-center hover:text-orange-400 hover:bg-white hover:border-orange-500 border-2">
+            <Link to="write">Write</Link>
+          </span>
+        </div>
       </div>
     </div>
   );
